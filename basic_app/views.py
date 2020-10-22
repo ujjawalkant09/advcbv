9639b2cb93dc2e6aa03e2d74875e7fc3c5283308
Ujjawal Kant <ujjawalkantojha@gmail.com>
2020-10-22 14:34:45 +0530

    CRUD Views Complete Code(Django)

--- basic_app/views.py
+++ basic_app/views.py
@@ -23,3 +23,20 @@ class SchoolDetailView(DetailView):
     context_object_name = 'school_details'
     model = models.School
     template_name = 'basic_app/school_detail.html'
+
+
+class SchoolCreateView(CreateView):
+    fields = ("name","principal","location")
+    model = models.School
+
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ("name","principal")
+    model = models.School
+
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
+
