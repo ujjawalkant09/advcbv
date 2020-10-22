772b0012e07690432bdadaa8c1014b6cf0f4a2b2
Ujjawal Kant <ujjawalkantojha@gmail.com>
2020-10-22 13:46:24 +0530

    162.Detail View Part Two

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView,ListView,DetailView
+from django.urls import reverse_lazy
+from django.http import HttpResponse
+from django.views.generic import (View,TemplateView,
+                                ListView,DetailView,
+                                CreateView,DeleteView,
+                                UpdateView)
 from basic_app import models
-
-# Create your views here.
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injectme'] = 'BASIC INJECTION!'
+        context  = super().get_context_data(**kwargs)
+        context['injectme'] = "Basic Injection!"
         return context
 
 class SchoolListView(ListView):
-    model = models.School 
+    model = models.School
 
 
 class SchoolDetailView(DetailView):
+    context_object_name = 'school_details'
     model = models.School
     template_name = 'basic_app/school_detail.html'
-
-    
-
-
